971b9994f271cfb6267885cf73e6b6e94c749f1b
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-19 18:03:43 -0300

    feat: criar interfaces TypeScript para autenticação e usuário

---
+++ src/types/auth.ts
@@ -0,0 +1,22 @@
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface User {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  birth: string;
+}
+
+export interface UserResponse {
+  id: number;
+  username: string;
+  email: string;
+}
