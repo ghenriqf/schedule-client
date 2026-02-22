db12daff0cbbfc125909335ee6fd8009dad58439
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-22 18:20:08 -0300

    feat: adicionar interceptor no axios para envio automático do token no header Authorization

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
 
+const AUTH_TOKEN_KEY = "@App:token";
+
 const axio = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+axio.interceptors.request.use((config) => {
+  const token = localStorage.getItem(AUTH_TOKEN_KEY);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axio;
